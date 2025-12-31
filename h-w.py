37ada44cecccb1c77258bde8165586f069301578
chetansri19-ai <chetansri19@gmail.com>
2025-12-31 14:47:07 +1100

    homework

---
+++ h-w.py
@@ -0,0 +1,7 @@
+#Writing a program to check the age entered by the user is between 10-20 or not.
+age = int(input("Enter your age: "))
+if age >= 10 and age <= 20:
+    print("Age is between 10 and 20.")
+else:
+    print("Age is not between 10 and 20.")
+
